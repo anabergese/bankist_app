878894b98b8a4859ee1d449d9b9089e8a2ceea26
ana bergese <violetagele22@gmail.com>
2021-09-07 10:46:34 +0200

    Calculate the total of outcomes and interests

--- script.js
+++ script.js
@@ -109,9 +109,22 @@ const createUsernnames = function (accs) {
 }
 
 const calDisplaySummary = function (movements) {
-  const incomes = movements.filter(mov => mov > 0).
-  reduce((acc, mov) => acc + mov, 0);
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => int >= 1)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumInterest.textContent = `${interest}€`;
 }
 
 calDisplaySummary(account1.movements)
